473518c68004ab1ef03550d8eb5d6bd5cd5e3736
Rostylsav Malaniuk <rostyslav.malaniuk@gmail.com>
2024-05-09 17:17:00 +0300

    add custom admin panel

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Thread, Message
+from django import forms
 
-# Register your models here.
+
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 1
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "sender":
+            thread_id = request.resolver_match.kwargs.get('object_id')
+            if thread_id:
+                thread = Thread.objects.get(pk=thread_id)
+                kwargs["queryset"] = thread.participants.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class ThreadAdminForm(forms.ModelForm):
+    class Meta:
+        model = Thread
+        fields = '__all__'
+
+    def clean_participants(self):
+        participants = self.cleaned_data['participants']
+        if len(participants) > 2:
+            raise forms.ValidationError('A Thread can have only up to 2 participants.')
+        return participants
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    form = ThreadAdminForm
+    list_display = ('id', 'created', 'updated')
+    inlines = [MessageInline]
+    readonly_fields = ('created', 'updated') 
+
+
+admin.site.register(Thread, ThreadAdmin)
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Thread(models.Model):
+    participants = models.ManyToManyField(User)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+
+@receiver(m2m_changed, sender=Thread.participants.through)
+def participants_changed(sender, instance, **kwargs):
+    if kwargs['action'] == 'pre_add':
+        participant_count = instance.participants.count() + len(kwargs['pk_set'])
+        if participant_count > 2:
+            raise ValidationError('A Thread can have only up to 2 participants.')
